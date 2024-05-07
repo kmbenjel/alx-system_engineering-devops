dc6e1f53dedbef30989b44ebcc7dfe56d7377989
Khalid Benjelloun <khalidmailing@gmail.com>
2024-05-07 10:11:30 +0100

    edit

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(\+?.+\b)\]\s\[to:(\+?.+\b)\]\s\[flags:(.+\b)\]/).join(",")
+puts ARGV[0].scan(/\[from:(.+\b)\]\s\[to:(.+\b)\]\s\[flags:(.+\b)\]/).join(",")
 
